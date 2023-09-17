b3c29e742d950d59d3064b51dc85e5e401140042
daproclaima <sebastien@devedanos.com>
2023-09-18 00:52:42 +0200

    feat: modify structure of /LedDriverGpioRpiGpioImplementation.js

--- src/adapters/raspberry/led/LedDriverImplementations/LedDriverGpioRpiGpioImplementation/LedDriverGpioRpiGpioImplementation.js
+++ src/adapters/raspberry/led/LedDriverImplementations/LedDriverGpioRpiGpioImplementation/LedDriverGpioRpiGpioImplementation.js
@@ -5,38 +5,60 @@ import gpio from 'rpi-gpio'
 export default class LedDriverGpioRpiGpioImplementation {
     driver = null
     logger = null
+    isLedToLit = false
+    isGpioToTearUp = false
 
     constructor({logger}) {
         this.logger = logger
         this.driver = gpio
 
-        gpio.setup(12, gpio.DIR_OUT, () => {
+        try {
+            gpio.setup(12, gpio.DIR_OUT, () => {
+                this.logger.log({
+                    level: 'info',
+                    message: `LedDriverGpioRpiGpioImplementation.construct set up pin 12`
+                })
+
+                if (this.isLedToLit === false) gpio.write(12, true, function (err) {
+                    if (err) throw err;
+
+                    this.logger.log({
+                        level: 'info',
+                        message: `LedDriverGpioRpiGpioImplementation.switchOnLed Written true to pin 12`
+                    })
+                });
+
+                if (this.isLedToLit === true) gpio.write(12, true, function (err) {
+                    if (err) throw err;
+
+                    this.logger.log({
+                        level: 'info',
+                        message: `LedDriverGpioRpiGpioImplementation.switchOnLed Written false to pin 12`
+                    })
+                })
+
+                if(this.isGpioToTearUp) gpio.destroy((error) => {
+                    if (error) throw error
+                    this.logger.log({
+                        level: 'info',
+                        message: `LedDriverGpioRpiGpioImplementation.tearUpGpios executed`
+                    })
+                });
+            });
+        } catch (error) {
             this.logger.log({
-                level: 'info',
-                message: `LedDriverGpioRpiGpioImplementation.construct set up pin 12`
+                level: 'error',
+                message: `LedDriverGpioRpiGpioImplementation error : `, error
             })
-        });
+        }
     }
 
     switchOnLed() {
-        gpio.write(12, true, function (err) {
-            if (err) throw err;
-
-            this.logger.log({
-                level: 'info',
-                message: `LedDriverGpioRpiGpioImplementation.switchOnLed Written true to pin 12`
-            })
-        });
+        this.isLedToLit = true
     }
 
     switchOffLed() {
-        gpio.write(12, true, function (err) {
-            if (err) throw err;
-            this.logger.log({
-                level: 'info',
-                message: `LedDriverGpioRpiGpioImplementation.switchOnLed Written false to pin 12`
-            })
-        })
+        this.isLedToLit = false
     }
 
     tearUpGpios() {
