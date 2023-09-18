9245ead23335b27626b45e01154c571243b31c8b
daproclaima <sebastien@devedanos.com>
2023-09-18 14:51:17 +0200

    refactor: update blink.js to use PIN_12 constant

--- src/adapters/raspberry/led/blink.js
+++ src/adapters/raspberry/led/blink.js
@@ -22,25 +22,29 @@
 
 import gpio from 'rpi-gpio'
 
-gpio.setup(12, gpio.DIR_OUT); //use GPIO pin 4, and specify that it is output
+const PIN_12= 12
+
+gpio.setup(PIN_12, gpio.DIR_OUT);
+gpio.setMode(gpio.MODE_RPI);
+//use GPIO pin 4, and specify that it is output
 const blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
 function blinkLED() { //function to start blinking
     let isLedLit = false
 
-    gpio.read(12, function (err, value) {
+    gpio.read(PIN_12, function (err, value) {
         if (err) throw err
 
-        console.log('the value of pin 12 is : ', value)
+        console.log(`the value of pin ${PIN_12} is : ${value}`)
         isLedLit = value
     })
 
     if (isLedLit === false) { //check the pin state, if the state is 0 (or off)
-        gpio.write(12, true, function (err) {
+        gpio.write(PIN_12, true, function (err) {
             if (err) throw err
         }) //set pin state to 1 (turn LED on)
     } else {
-        gpio.write(12, false, function (err) {
+        gpio.write(PIN_12, false, function (err) {
             if (err) throw err
         }); //set pin state to 0 (turn LED off)
     }
@@ -48,7 +52,7 @@ function blinkLED() { //function to start blinking
 
 function endBlink() { //function to stop blinking
     clearInterval(blinkInterval); // Stop blink intervals
-    gpio.write(12, false, err => {
+    gpio.write(PIN_12, false, err => {
         if (err) throw err
     }); // Turn LED off
     gpio.destroy(() => {
