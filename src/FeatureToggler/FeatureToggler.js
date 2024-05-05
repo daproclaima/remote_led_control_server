2920eebd428c381d147e17683fff6cc5f6867613
daproclaima <sebastien@devedanos.com>
2024-05-05 13:29:46 +0200

    🚚(server) remove src/constants/OperatingSystems
    rename src/constants/OperatingSystems/OS.js
    into src/FeatureToggler/OPERATING_SYSTEMS.js
    change src/FeatureToggler/FeatureToggler.js
    imports

--- src/FeatureToggler/FeatureToggler.js
+++ src/FeatureToggler/FeatureToggler.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import {FEATURE_GPIO} from "./FEATURES.js";
-import {OS_TYPE} from "../constants/OperatingSystems/OS.js";
+import {OS_TYPE} from "./OPERATING_SYSTEMS.js";
 
 export default class FeatureToggler {
     features = {
