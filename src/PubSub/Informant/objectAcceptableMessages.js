33890ac523633e526fc9fe9ec17e7cba336f58b4
daproclaima <sebastien@devedanos.com>
2024-05-07 22:38:47 +0200

    refactor: WsWebSocketImplementation.listen receive callbacks

--- src/PubSub/Informant/objectAcceptableMessages.js
+++
@@ -1,7 +0,0 @@
-import PubSubMessage from "../PubSubMessage.js";
-
-export const objectAcceptableMessages = {
-    [PubSubMessage.switchOnLed]: PubSubMessage.switchOnLed,
-    [PubSubMessage.switchOffLed]: PubSubMessage.switchOffLed,
-    [PubSubMessage.terminateGpioLed]: PubSubMessage.terminateGpioLed
-}
--- src/PubSub/Informant/objectRepliesForMessages.js
+++
@@ -1,8 +0,0 @@
-import PubSubMessage from "../PubSubMessage.js";
-import PubSubReply from "../PubSubReply.js";
-
-export const objectRepliesForMessages = {
-    [PubSubMessage.switchOnLed]: PubSubReply.switchedOnLed,
-    [PubSubMessage.switchOffLed]: PubSubReply.switchedOffLed,
-    [PubSubMessage.terminateGpioLed]: PubSubReply.terminatedGpioLed
-}
